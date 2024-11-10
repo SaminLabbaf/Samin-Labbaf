03899dc368e82fe4a97d5ba99313341ee7830ccb
Samin Labbaf <samin.labbaf@georgebrown.ca>
2024-11-10 18:17:23 -0500

    added layout, header and global style

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,24 @@
+import './styles/global.css'
+import Image from 'next/image'
+import logo from '../public/images/logo.svg'
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <header>
+            <Image src={logo} alt='logo'/>
+            <nav>
+              <ul>
+                <li><a href="#">Work & Skills</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </nav>
+            <div>dummy element</div>
+          </header>
+          {children}</body>
       </html>
     )
   }
