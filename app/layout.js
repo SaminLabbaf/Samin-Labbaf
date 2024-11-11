dbb3d6ba96e6dab3dcf9dfaaff59195054b1bf81
Samin Labbaf <samin.labbaf@georgebrown.ca>
2024-11-11 01:10:37 -0500

    added footer section in layout

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,49 @@
 import './styles/global.css'
 import Image from 'next/image'
 import logo from '../public/images/logo.svg'
+import instagram from '../public/images/instagram-logo.svg'
+import x from '../public/images/x-logo.svg'
+import linkedin from '../public/images/linkedin-logo.svg'
+import email from '../public/images/mail-icon.svg'
 
 export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>
-          <header>
-            <Image src={logo} alt='logo'/>
-            <nav>
-              <ul>
-                <li><a href="#">Work & Skills</a></li>
-                <li><a href="#">About Me</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </nav>
-            <div>dummy element</div>
-          </header>
-          {children}</body>
-      </html>
-    )
-  }
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Image src={logo} alt='logo' />
+          <nav>
+            <ul>
+              <li><a href="#">Work & Skills</a></li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+          <div>dummy element</div>
+        </header>
+
+        {children}
+
+        <footer>
+          <div className='footerContent'>
+            <ul>
+              <li><a href="#">Work & Skills</a></li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <ul>
+              <li><a href='#'><Image src={instagram}/></a></li>
+              <li><a href='#'><Image src={x}/></a></li>
+              <li><a href='#'><Image src={linkedin}/></a></li>
+              <li><a href='#'><Image src={email}/></a></li>
+            </ul>
+            <span className='copyRight'>© 2024 Samin Labbaf, All rights preserved.</span>
+          </div>
+          
+        </footer>
+      </body>
+    </html>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
-
+export const metadata = {
+  title: 'Samin Labbaf',
+  description: 'Samin Labbaf Portfolio',
+}
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <>
+  <main className="mainPage">
+  <h1>Hello, I'm Samin</h1>
+  </main>
+  </>
 }
