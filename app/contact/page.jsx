06d4abb74e8556c1b5a257c0f13f5068f0f4d98c
Samin Labbaf <samin.labbaf@georgebrown.ca>
2024-12-07 23:50:21 -0500

    added MUI and finished contact page just UI

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,8 @@ import Image from 'next/image';
 import '../styles/home.style.css'
 import me from '../../public/images/me.png';
 import './contact.style.css';
-import { TextField, Box, FormGroup, FormLabel } from '@mui/material';
+import { TextField, Box, FormGroup, FormLabel, Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send'
 
 export const metadata = {
     title: 'Contact',
@@ -21,21 +22,24 @@ export default function Contact() {
                     className='form'
                 >
                     <FormGroup>
-                    <FormLabel>Email</FormLabel>
-                    <TextField
-                        id="email"
-                        variant="outlined"
-                        aria-label='email input'
-                        placeholder='Your Email'
-                    />
-                     <FormLabel color='error'>Message</FormLabel>
-                    <TextField
-                        id="message"
-                        multiline
-                        rows={4}
-                        placeholder='Your Message'
-                    />
+                        <FormLabel>Email</FormLabel>
+                        <TextField
+                            id="email"
+                            variant="outlined"
+                            aria-label='email input'
+                            placeholder='Your Email'
+                        />
+                        <FormLabel color='error'>Message</FormLabel>
+                        <TextField
+                            id="message"
+                            multiline
+                            rows={4}
+                            placeholder='Your Message'
+                        />
                     </FormGroup>
+                    <Button type='submit' variant="contained" endIcon={<SendIcon />}>
+                        Send
+                    </Button>
                 </Box>
             </section>
             <section>
--- theme.js
+++ theme.js
@@ -1,10 +1,26 @@
-'use client';
+'use client'
+
 import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
 
 const theme = createTheme({
-  typography: {
-    fontFamily: 'var(--font-roboto)',
+  palette: {
+    primary: {
+      main: '#7F01D8',
+    },
+    secondary: {
+      main: green[500],
+    },
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          background: 'linear-gradient(90deg, rgba(69, 102, 234, 0.98) 0%, rgba(153, 17, 243, 0.98) 100%)'
+        }
+      }
+    }
+  }
 });
 
-export default theme;
+export default theme
