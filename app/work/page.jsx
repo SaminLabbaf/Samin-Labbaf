e10485ac3441240d43084f90a49fd1baa609cc93
Samin Labbaf <samin.labbaf@georgebrown.ca>
2024-12-09 04:09:13 -0500

    added skills and career path section

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -3,6 +3,15 @@ import '../styles/home.style.css'
 import antcoders from '../../public/images/antcoders.svg';
 import dexis from '../../public/images/dexis.svg';
 import tod from '../../public/images/tod.svg';
+import htmlIcon from '../../public/images/html.svg';
+import cssIcon from '../../public/images/css.svg';
+import JSIcon from '../../public/images/js.svg';
+import MuiIcon from '../../public/images/mui.svg';
+import NextJSIcon from '../../public/images/nextjs.svg';
+import ReactJSIcon from '../../public/images/reactjs.svg';
+import persistence from '../../public/images/persistence.svg';
+import creativity from '../../public/images/creativity.svg';
+import details from '../../public/images/details.svg';
 import { Container, Typography } from '@mui/material';
 import './work.style.css'
 
@@ -49,6 +58,57 @@ export default function Work() {
                         </div>
                         <hr />
                     </div>
+                </section>
+                <section id='skillSection'>
+                    <h2>Skills and Languages</h2>
+                    <div className="skillSet">
+                        <div className='skill'>
+                            <Image src={htmlIcon} alt='html 5 icon' />
+                            <h4>HTML 5</h4>
+                        </div>
+                        <div className='skill' id='css'>
+                            <Image src={cssIcon} alt='css 3 icon' />
+                            <h4>CSS 3</h4>
+                        </div>
+                        <div className='skill'>
+                            <Image src={JSIcon} alt='javascript icon' />
+                            <h4>JavaScript</h4>
+                        </div>
+                    </div>
+                    <h2>Frameworks & Libraries</h2>
+                    <div className="skillSet">
+                        <div className='skill'>
+                            <Image src={NextJSIcon} alt='Next JS icon' />
+                            <h4>NextJS</h4>
+                        </div>
+                        <div className='skill'>
+                            <Image src={ReactJSIcon} alt='React JS icon' />
+                            <h4>ReactJS</h4>
+                        </div>
+                        <div className='skill'>
+                            <Image src={MuiIcon} alt='material ui icon' />
+                            <h4>Material UI</h4>
+                        </div>
+                    </div>
+                    <h2>Soft Skills</h2>
+                    <div className="skillSet">
+                        <div className='skill'>
+                            <Image src={persistence} alt='persistence skill' />
+                            <h4>Persistence</h4>
+                        </div>
+                        <div className='skill'>
+                            <Image src={creativity} alt='creativity skill' />
+                            <h4>Creativity</h4>
+                        </div>
+                        <div className='skill'>
+                            <Image src={details} alt='Precision' />
+                            <h4>Precision</h4>
+                        </div>
+                    </div>
+                </section>
+                <section id='careerPath'>
+                    <h1>My Career Path</h1>
+                    <p>Non dolore magna ut nisi duis magna minim nulla anim laborum cillum incididunt. Minim excepteur sit dolore elit labore magna aute consectetur dolor cillum non aliqua sint labore laboris tempor occaecat laboris aliqua. Irure non ullamco sit amet aliquip officia duis e</p>
                 </section>
             </Container>
         </main>
